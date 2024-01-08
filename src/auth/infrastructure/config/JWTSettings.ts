1dab04c02462139b40989697a6d03078254abc42
Carlos Sosa <carlos.alberto.sosa.perera@gmail.com>
2024-01-07 18:12:35 -0600

    renaming enviroment variables to be more semantic

--- src/auth/infrastructure/config/JWTSettings.ts
+++ src/auth/infrastructure/config/JWTSettings.ts
@@ -2,5 +2,5 @@ import { config } from "dotenv";
 
 config();
 const SECRET_KEY = `${process.env.SECRET_ACCESS_KEY}`;
-const EXPIRES_IN = `${process.env.SESSION_TOKEN_EXPIRES_IN}`;
+const EXPIRES_IN = `${process.env.JWT_EXPIRES_IN}`;
 export default { SECRET_KEY, EXPIRES_IN };
